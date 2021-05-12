57044ab192ce545b0973068e252990f65400d112
FELIPE PRODOSSIMO <felipe.prodossimo@senacsp.edu.br>
2021-05-11 21:44:49 -0300

    Incluindo css, js, cabeçalho.php e rodape.php

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php include "cabecalho.php"; ?> 
+      
+        <div class="conteudo"> 
+            <h2>Conteúdo da aplicação</h2>
+        </div>  
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App Consultas</title>
-</head>
-
-<body>
-    <header>
-        <main>
-            <h1>APP Consultas</h1>
-            <hr>
-        </main>
-    </header>
-    <nav>
-        <a href="#"><img src="img/medico.png">Cadastro de médicos</a>
-        <a href="#"><img src="img/paciente.png">Cadastro de pacientes</a>
-        <a href="#"><img src="img/calendario.png">Agendamento de consulta</a>
-    </nav>
-    <div class="conteudo"> 
-        <h2>Conteúdo da aplicação</h2>
-    </div>    
-<footer>
-    &copy; Todos os direitos reservados
-</footer>
-
-</body>
-
-</html>
+<?php include "rodape.php"; ?>
---
+++ rodape.php
@@ -0,0 +1,10 @@
+<footer>
+            &copy; Todos os direitos reservados
+        </footer>
+    </main>
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.min.js"></script>
+
+</body>
+
+</html>
