46b1b07decd2a76541b25f3afcb2f7c018658431
Felipe Prodossimo <felipe.profissional4@gmail.com>
2021-05-20 22:35:02 -0300

    Tabela de agenda concluída!

---
+++ agenda/agenda-alterar.php
@@ -0,0 +1,29 @@
+<?php 
+include "../include/conexao.php";
+
+$id = $_POST['id'];
+$data = $_POST['data'];
+$hora = $_POST['hora'];
+$id_medico = $_POST['id_medico'];
+$sala = $_POST['sala'];
+$id_paciente = $_POST['id_paciente'];
+
+$sqlAlterar = "UPDATE tb_agenda SET 
+data = '{$data}', 
+hora = '{$hora}', 
+id_medico = '{$id_medico}', 
+sala = '{$sala}', 
+id_paciente = '{$id_paciente}' 
+WHERE tb_agenda.id = {$id}";
+    
+$resultado = mysqli_query($conexao , $sqlAlterar);
+
+if($resultado){
+    echo "Alterado com sucesso!<br>";
+    echo "<a href='agenda-listar.php'>Voltar</a>";
+}else{
+    echo "Ocorreu algum erro";
+}
+
+
+?>
--- agenda/agenda-formulario-alterar.php
+++ agenda/agenda-formulario-alterar.php
@@ -1,59 +1,74 @@
 <?php
 include "../include/cabecalho.php"; 
 
-$id_paciente = $_GET['id'];
+$id = $_GET['id'];
 
 include "../include/conexao.php";
-$sqlBusca = "SELECT * FROM tb_pacientes WHERE id={$id_paciente};";
+$sqlBusca = "SELECT * FROM tb_agenda WHERE id={$id};";
 
-$listaDePacientes = mysqli_query($conexao , $sqlBusca);
+$listaDaAgenda = mysqli_query($conexao , $sqlBusca);
 
-$nome = $telefone = $nascimento = $convenio = $diagnostico = "";
+$data = $hora = $id_medico = $sala = $id_paciente = "";
 
-while($paciente = mysqli_fetch_assoc($listaDePacientes)){
-    $nome = $paciente['nome'];
-    $telefone = $paciente['telefone'];
-    $nascimento = $paciente['nascimento'];
-    $convenio = $paciente['convenio'];
-    $diagnostico = $paciente['diagnostico'];
+while($agenda = mysqli_fetch_assoc($listaDaAgenda)){
+    $data = $agenda['data'];
+    $hora = $agenda['hora'];
+    $id_medico = $agenda['id_medico'];
+    $sala = $agenda['sala'];
+    $id_paciente = $agenda['id_paciente'];
 }
 ?>
 
-
 <div class="container">
     <div class="row">
-        <form class=text-center name="formulario-inserir-agenda" action="agenda-alterar.php" method="post">    
-        <input name="id" type="hidden" value="<?php echo $_GET['id']; ?>">
-        <div class="col">
-                    <label class="form-label">Nome:</label>
-                    <input name="nome" class="form-control" value="<?php echo $nome ; ?>">
-                </div>
-                <div class="col">
-                    <label class="form-label">Telefone:</label>
-                    <input name="telefone" class="form-control" value="<?php echo $telefone ; ?>">
-                </div>
-                <div class="col">
-                    <label class="form-label">Data de Nascimento:</label>
-                    <input class="form-control text-center" id="nascimento" 
-                    name="nascimento" type="date" <?php echo $nascimento ; ?>>
-                    </div>
-                <div class="form-check form-switch">
-                    <label class="form-check-label" for="flexSwitchCheckDefault">Convênio:</label>
-                    <?php $marcado = ""; 
-                    if($convenio == "sim"){
-                        $marcado = "checked";
-                    }?>
-                    <input class="form-check-input" value="sim" type="checkbox" name="convenio" 
-                    style="font-size: 20px" <?php echo $marcado;?>>
-                </div>
-                <div class="form-floating">
-                    <textarea class="form-control" <?php echo $diagnostico ; ?> name="diagnostico"
-                    style="height: 100px"></textarea>
-                    <label>Diagnóstico:</label>
-                </div>
+        <form name="altera-agenda" method="post" action="agenda-alterar.php">
+        <input name="id" type="hidden" value="<?php echo $id;?>">
+            <div class="col">
+                <label class="form-label">Data:</label>
+                <input class="form-control" type="date" name="data" value="<?php echo $data; ?>">
+            </div>
+            <div class="col">
+                <label class="form-label">Hora:</label>
+                <input class="form-control" type="time" name="hora" value="<?php echo $hora; ?>">
+            </div>
+            <div class="col">
+                <label class="form-label">Médico:</label>
+                <select name="id_medico" class="form-select form-select-lg mb-3" value="<?php echo $id_medico; ?>">
+                    <?php 
+                        $sqlBuscaMedicos = "SELECT * FROM tb_medicos";
+                        $listaDeMedicos = mysqli_query($conexao , $sqlBuscaMedicos);
+
+                        while($medico = mysqli_fetch_assoc($listaDeMedicos)){
+                            echo "<option value='{$medico['id']}'>";
+                            echo $medico['nome'];
+                            echo "</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="col">
+                <label class="form-label">Sala:</label>
+                <input class="form-control" name="sala" value="<?php echo $sala; ?>">
+            </div>
+            <div class="col">
+                <label >Paciente:</label>
+                <select name="id_paciente" class="form-select form-select-lg mb-3" value="<?php echo $id_paciente; ?>">
+                    <?php 
+                        $sqlBuscaPacientes = "SELECT * FROM tb_pacientes";
+                        $listaDePacientes = mysqli_query($conexao , $sqlBuscaPacientes);
+
+                        while($paciente = mysqli_fetch_assoc($listaDePacientes)){
+                            echo "<option value='{$paciente['id']}'>";
+                            echo $paciente['nome'];
+                            echo "</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <p>
                 <button type="submit" class="btn btn-primary btn-lg">Salvar</button>
-                </div>
+            </p>
+
         </form>
-    </div>
-</div>
+
 <?php include "../include/rodape.php"; ?>
