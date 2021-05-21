6c35ca1bd6a27234d4d4d1a40e265d5fe3f84de3
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2021-05-20 22:38:20 -0300

    Tabela da Agenda concluída!

---
+++ agenda/agenda-excluir.php
@@ -0,0 +1,18 @@
+<?php
+
+include "../include/conexao.php";
+
+$id = $_GET['id'];
+
+$sqlExcluir = "DELETE FROM tb_agenda WHERE id = {$id}";
+
+$resultado = mysqli_query($conexao , $sqlExcluir);
+
+if($resultado){
+    echo "Excluido com sucesso <br>";
+    echo "<a href='agenda-listar.php'> Voltar</a>";
+}else{
+    echo "Ocorreu algum problema";
+}
+
+?>
